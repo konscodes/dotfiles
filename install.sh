aa0c722f7bd10440ba13b0f5883521a432e402bf
Kons <katashi.rain@gmail.com>
2023-04-03 23:52:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-#
-# Installation script for dotfiles
+## Installation script for dotfiles ##
+set -e  # enable the 'errexit' option
+cd $HOME
+
 
 # Set environment variables
-cd $HOME
 DOTFILES_ROOT=$(pwd -P)
+DOTFILES_GIT_CMD="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
 
 # Create information output functions
 info () {
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
 success () {
@@ -21,7 +20,35 @@ success () {
 
 fail () {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
+  echo
   exit
 }
 
+
+# Create main functions
+install_zsh() {
+  info 'installing zsh'
+  # Install zsh on Ubuntu
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      sudo apt-get update
+      sudo apt-get install -y zsh
+  # Install zsh on macOS using Homebrew
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+      brew install zsh
+  fi
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+  mkdir $HOME/.dotfiles
+  echo ".dotfiles" >> .gitignore
+  git clone --bare https://github.com/konscodes/dotfiles.git $HOME/.dotfiles
+  $DOTFILES_GIT_CMD config --local status.showUntrackedFiles no
+  $DOTFILES_GIT_CMD checkout
+  $DOTFILES_GIT_CMD restore .zshrc
+  $DOTFILES_GIT_CMD status
+}
+
+
+# Run functions
+info 'runnning the script'
