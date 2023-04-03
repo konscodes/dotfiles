52239e773b0e3135b1986c417e404826d6852174
Kons <katashi.rain@gmail.com>
2023-04-03 13:27:47 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Installation script for dotfiles
+
+# Set environment variables
+cd $HOME
+DOTFILES_ROOT=$(pwd -P)
+
+# Create information output functions
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
